76d544a0f6eccabb8f1e589663e0defb4fac5aa5
Yassin Hajjej <ysnhajjej@gmail.com>
2024-05-01 17:03:41 -0700

    MVP: wip on Photos and corssuser visibility

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Lesson, Comments
+from .models import Lesson, Comments, Photo
 
 # Register your models here.
 admin.site.register(Lesson)
 admin.site.register(Comments)
+admin.site.register(Photo)
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,3 @@
-from django.forms import BaseModelForm
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -9,10 +7,10 @@ from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from .models import Lesson, Comments
+from .models import Lesson, Comments, Photo
 from .forms import CommentForm
 from datetime import date
+import boto3, uuid
 
 # Create your views here.
 @login_required
@@ -26,8 +24,9 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def lessons_index(request):
-    lessons = Lesson.objects.all()
+    lessons = Lesson.objects.filter(user=request.user)
     return render(request, 'lessons/index.html', {
         'lessons': lessons,
     })
@@ -36,7 +35,6 @@ def lessons_index(request):
 def lesson_detail(request, lesson_id):
     lesson = Lesson.objects.get(id=lesson_id)
     comments = lesson.comments.all()
-    print(comments)
     comment_form = CommentForm()
     return render(request, 'lessons/detail.html', {
         'lesson': lesson,
@@ -44,30 +42,37 @@ def lesson_detail(request, lesson_id):
         'comment_form': comment_form,
     })
 
+
 class LessonCreate(CreateView):
     model = Lesson
-    fields = '__all__'
+    fields = ['title', 'description', 'content', 'difficulty_level']
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-    
+
+import os  
 def add_photo(request, lesson_id):
     photo_file = request.FILES.get('photo-file', None)
     if photo_file:
         s3 = boto3.client('s3')
         key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
         try:
-            s3.upload_fileobj(photo_file, BUCKET, key)
-            url = f"{S3_BASE_URL}{key}"
+            s3.upload_fileobj(photo_file, os.environ['S3_BUCKET'], key)
+            url = f"{os.environ['S3_BASE_URL']}{key}"
             Photo.objects.create(url=url, lesson_id=lesson_id)
-        except:
-            print('An error occurred uploading file to S3')
+        except Exception as e:
+            print('Error uploading to S3')
+            print('Exception message: ', e)
     return redirect('lesson_detail', lesson_id=lesson_id)
 
 class LessonUpdate(UpdateView):
     model = Lesson 
-    fields = '__all__'
+    fields = ['title', 'description', 'content', 'difficulty_level']
+
+    # def form_valid(self, form):
+    #     form.instance.user = self.request.user
+    #     return super().form_valid(form)
 
 class LessonDelete(DeleteView):
     model = Lesson
